eacf8cbad8fdfd4b2ba0242764830400abc91cba
Tom Rini <trini@ti.com>
2011-09-28 12:50:52 -0700

    am335x: Make it boot again
    - Correct CONFIG_SYS_TEXT_BASE to be where u-boot-min was running it
    - Update CONFIG_SYS_INIT_SP_ADDR to use GENERATED_GBL_DATA_SIZE
    - Borrow v7 cache flush functions from omap3 verbatim
    - Update dram_init / dram_init_banksize for relocation.
    - Turn on relocation of u-boot
    - Enable DEBUG for now
    - REVERT aaeb0a890a050b58be87fa2b165eec5fa947dc86
    - Disable enet for the moment
    
    Signed-off-by: Tom Rini <trini@ti.com>

--- include/configs/am335x_evm.h
+++ include/configs/am335x_evm.h
@@ -15,10 +15,10 @@
 
 #define CONFIG_ARMV7
 
+#define DEBUG
+
 #include <asm/arch/cpu.h>		/* get chip and board defs */
 #include <asm/arch/hardware.h>
-
-#define CONFIG_SYS_ARM_WITHOUT_RELOC	1
 
 #define CONFIG_AM335X_HSMMC_INSTANCE	0	/* 0 - MMC0, 1 - MMC1 */
 
@@ -200,8 +200,6 @@
 #endif /* CONFIG_AM335X_MIN_CONFIG */
 
 #define CONFIG_DISPLAY_BOARDINFO
-#define CONFIG_SYS_GBL_DATA_SIZE	128	/* size in bytes reserved for
-						   initial data */
 #define CONFIG_MISC_INIT_R		1
 #define CONFIG_SYS_AUTOLOAD		"yes"
 #define CONFIG_CMD_CACHE
@@ -233,6 +231,13 @@
 #define PHYS_DRAM_1			0x80000000	/* DRAM Bank #1 */
 #define PHYS_DRAM_1_SIZE		0x10000000 /*(0x80000000 / 8) 256 MB */
 
+#define CONFIG_SYS_SDRAM_BASE		PHYS_DRAM_1
+#define CONFIG_SYS_INIT_RAM_ADDR	SRAM0_START
+#define CONFIG_SYS_INIT_RAM_SIZE	SRAM0_SIZE
+#define CONFIG_SYS_INIT_SP_ADDR		(CONFIG_SYS_INIT_RAM_ADDR + \
+					 CONFIG_SYS_INIT_RAM_SIZE - \
+					 GENERATED_GBL_DATA_SIZE)
+
  /* Platform/Board specific defs */
 #define CONFIG_SYS_CLK_FREQ		24000000
 #define CONFIG_SYS_TIMERBASE		0x48040000	/* Use Timer2 */
@@ -257,8 +262,10 @@
 #define CONFIG_CONS_INDEX		1
 #define CONFIG_SYS_CONSOLE_INFO_QUIET
 
+#define CONFIG_NO_ETH
 #if defined(CONFIG_NO_ETH)
 # undef CONFIG_CMD_NET
+# undef CONFIG_CMD_NFS
 #else
 # define CONFIG_CMD_DHCP
 # define CONFIG_CMD_PING
@@ -392,9 +399,5 @@
 
 /* Unsupported features */
 #undef CONFIG_USE_IRQ
-
-/* additions for new relocation code, must added to all boards */
-#define CONFIG_SYS_SDRAM_BASE		PHYS_DRAM_1
-#define CONFIG_SYS_INIT_SP_ADDR		((SRAM0_START + SRAM0_SIZE - SRAM_GPMC_STACK_SIZE) - CONFIG_SYS_GBL_DATA_SIZE)
 
  #endif	/* ! __CONFIG_AM335X_EVM_H */
