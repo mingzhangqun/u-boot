jlquant/u-boot-omap3

ti816x: Change TEXT_BASE
To avoid issues with larger kernel images change the TEXT_BASE
to leave a space of 7MB on top.

Signed-off-by: Vaibhav Bedia <vaibhav.bedia@ti.com>

board/ti/ti8168/config.mk
@@ -21,7 +21,7 @@ TI_IMAGE = u-boot.noxip.bin
 TI_DEVICE = ti81xx
 # ROM code will load u-boot to this address
 TI_LOAD_ADDR = 0x40400000
-TEXT_BASE = 0x80300000
+TEXT_BASE = 0x80700000
 CROSS_COMPILE:= arm-none-linux-gnueabi-
 LDSCRIPT:=board/ti/ti8168/u-boot.lds
 # Over-ride the macros if supplied from the Makefile

mingzhangqun/u-boot

board/ti/ti8168/config.mk
@@ -21,6 +21,6 @@ TI_IMAGE = u-boot.noxip.bin
 TI_DEVICE = ti81xx
 # ROM code will load u-boot to this address
 TI_LOAD_ADDR = 0x40400000
-CONFIG_SYS_TEXT_BASE = 0x80300000
+CONFIG_SYS_TEXT_BASE = 0x80700000
 # Over-ride the macros if supplied from the Makefile
 sinclude $(OBJTREE)/board/$(BOARDDIR)/config.tmp