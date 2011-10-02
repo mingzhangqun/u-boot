jlquant/u-boot-omap3

ti81xx: Initial support
This patch adds support for ti81xx class of processors.
It also adds support for ti8168 based EVMs

Signed-off-by: Hemant Pedanekar <hemantp@ti.com>
Signed-off-by: Vaibhav Bedia <vaibhav.bedia@ti.com>

Makefile
@@ -3196,6 +3196,17 @@ s5p_goni_config:	unconfig
 smdkc100_config:	unconfig
 	@$(MKCONFIG) $(@:_config=) arm arm_cortexa8 smdkc100 samsung s5pc1xx
 
+ti8168_evm_min_config:	unconfig
+	@mkdir -p $(obj)include
+	@echo "#define CONFIG_TI81XX"	>>$(obj)include/config.h
+	@echo "#define CONFIG_TI816X"	>>$(obj)include/config.h
+	@if [ "$(findstring _min_,$@)" ] ; then \
+		echo "#define CONFIG_SYS_NO_FLASH"    >>$(obj)include/config.h ; \
+		echo "#define CONFIG_NO_ETH"    >>$(obj)include/config.h ; \
+		echo "Setting up TI8168 minimal build..." ; \
+	fi;
+	@$(MKCONFIG) -a ti8168_evm arm arm_cortexa8 ti8168 ti ti81xx
+
 #########################################################################
 ## XScale Systems
 #########################################################################

board/ti/ti8168/config.mk
@@ -1,0 +1,28 @@
+#
+# Copyright (C) 2009 Texas Instruments Incorporated - http://www.ti.com/
+#
+# See file CREDITS for list of people who contributed to this
+# project.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation version 2.
+#
+# This program is distributed "as is" WITHOUT ANY WARRANTY of any
+# kind, whether express or implied; without even the implied warranty
+# of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+
+sinclude $(OBJTREE)/board/$(BOARDDIR)/config.tmp
+# Output image name. Used only in case of non-xip. For NOR boot u-boot.bin
+# could be used.
+TI_IMAGE = u-boot.noxip.bin
+# This will be used by mkimage extension to select header for image
+TI_DEVICE = ti81xx
+# ROM code will load u-boot to this address
+TI_LOAD_ADDR = 0x40400000
+TEXT_BASE = 0x80300000
+CROSS_COMPILE:= arm-none-linux-gnueabi-
+LDSCRIPT:=board/ti/ti8168/u-boot.lds
+

mingzhangqun/u-boot

Makefile
@@ -868,6 +868,17 @@ tx25_config	: unconfig
 	@echo "CONFIG_NAND_U_BOOT = y" >> $(obj)include/config.mk
 	@$(MKCONFIG) $@ arm arm926ejs tx25 karo mx25
 
+ti8168_evm_min_config:	unconfig
+	@mkdir -p $(obj)include
+	@echo "#define CONFIG_TI81XX"	>>$(obj)include/config.h
+	@echo "#define CONFIG_TI816X"	>>$(obj)include/config.h
+	@if [ "$(findstring _min_,$@)" ] ; then \
+		echo "#define CONFIG_SYS_NO_FLASH"    >>$(obj)include/config.h ; \
+		echo "#define CONFIG_NO_ETH"    >>$(obj)include/config.h ; \
+		echo "Setting up TI8168 minimal build..." ; \
+	fi;
+	@$(MKCONFIG) -a ti8168_evm arm armv7 ti8168 ti ti81xx
+
 #########################################################################
 ## XScale Systems
 #########################################################################

board/ti/ti8168/config.mk
@@ -1,0 +1,25 @@
+#
+# Copyright (C) 2009 Texas Instruments Incorporated - http://www.ti.com/
+#
+# See file CREDITS for list of people who contributed to this
+# project.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation version 2.
+#
+# This program is distributed "as is" WITHOUT ANY WARRANTY of any
+# kind, whether express or implied; without even the implied warranty
+# of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+
+sinclude $(OBJTREE)/board/$(BOARDDIR)/config.tmp
+# Output image name. Used only in case of non-xip. For NOR boot u-boot.bin
+# could be used.
+TI_IMAGE = u-boot.noxip.bin
+# This will be used by mkimage extension to select header for image
+TI_DEVICE = ti81xx
+# ROM code will load u-boot to this address
+TI_LOAD_ADDR = 0x40400000
+CONFIG_SYS_TEXT_BASE = 0x80300000