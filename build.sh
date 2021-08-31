42e1a46b0d95e0a1123c0600d35ede011e90d707
Robert Nelson <robertcnelson@gmail.com>
2021-08-31 17:04:32 -0500

    build.sh: resync configs/j721e_evm_r5_defconfig
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- build.sh
+++ build.sh
@@ -4,12 +4,10 @@
 #binutils-arm-linux-gnueabihf binutils-aarch64-linux-gnu
 #gcc-arm-linux-gnueabihf gcc-aarch64-linux-gnu
 
-echo "make CROSS_COMPILE=arm-linux-gnueabihf- distclean"
-make CROSS_COMPILE=arm-linux-gnueabihf- distclean
+make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- distclean
+make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- menuconfig
 
-echo "make CROSS_COMPILE=arm-linux-gnueabihf- j721e_evm_r5_defconfig O=/tmp/r5"
-make CROSS_COMPILE=arm-linux-gnueabihf- j721e_evm_r5_defconfig O=/tmp/r5
-
-echo "make -j2 CROSS_COMPILE=arm-linux-gnueabihf- O=/tmp/r5"
-make -j2 CROSS_COMPILE=arm-linux-gnueabihf- O=/tmp/r5
-
+make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- j721e_evm_r5_defconfig
+make ARCH=arm -j2 CROSS_COMPILE=arm-linux-gnueabihf-
+make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- savedefconfig
+cp -v defconfig ./configs/j721e_evm_r5_defconfig
