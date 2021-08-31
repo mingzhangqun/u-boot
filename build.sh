4acdd6bd3aaec41fd8d5872769b2f078a3eece0c
Robert Nelson <robertcnelson@gmail.com>
2021-08-31 17:14:34 -0500

    add basic build.sh with config sync
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- build.sh
+++ build.sh
@@ -5,9 +5,17 @@
 #gcc-arm-linux-gnueabihf gcc-aarch64-linux-gnu
 
 make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- distclean
+make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- j721e_evm_r5_defconfig
 make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- menuconfig
 
-make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- j721e_evm_r5_defconfig
 make ARCH=arm -j2 CROSS_COMPILE=arm-linux-gnueabihf-
 make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- savedefconfig
 cp -v defconfig ./configs/j721e_evm_r5_defconfig
+
+make ARCH=arm CROSS_COMPILE=aarch64-linux-gnu- distclean
+make ARCH=arm CROSS_COMPILE=aarch64-linux-gnu- j721e_evm_a72_defconfig
+make ARCH=arm CROSS_COMPILE=aarch64-linux-gnu- menuconfig
+
+make ARCH=arm -j2 CROSS_COMPILE=aarch64-linux-gnu- ATF=ti-blobs/bl31.bin TEE=ti-blobs/tee-pager_v2.bin DM=ti-blobs/ipc_echo_testb_mcu1_0_release_strip.xer5f
+make ARCH=arm CROSS_COMPILE=aarch64-linux-gnu- savedefconfig
+cp -v defconfig ./configs/j721e_evm_a72_defconfig
