f4345d60003933c113d1e18a9c7851c60902066f
Robert Nelson <robertcnelson@gmail.com>
2021-10-13 16:53:37 -0500

    install.sh: extra careful with boot files
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- install.sh
+++ install.sh
@@ -7,7 +7,27 @@ if ! id | grep -q root; then
 fi
 
 if [ -d /boot/firmware/ ] ; then
+	if [ -f /boot/firmware/tiboot3.bin ] ; then
+		rm -f /boot/firmware/tiboot3.bin || true
+		sync
+	fi
+
+	if [ -f /boot/firmware/tispl.bin ] ; then
+		rm -f /boot/firmware/tispl.bin || true
+		sync
+	fi
+
+	if [ -f /boot/firmware/u-boot.img ] ; then
+		rm -f /boot/firmware/u-boot.img || true
+		sync
+	fi
+
 	cp -v ./ti-blobs/tiboot3.bin /boot/firmware/
+	sync
+
 	cp -v ./ti-blobs/tispl.bin /boot/firmware/
+	sync
+
 	cp -v ./ti-blobs/u-boot.img /boot/firmware/
+	sync
 fi
