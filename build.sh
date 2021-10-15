7d958a6658fb7f421a94b36771a49170d143196f
Robert Nelson <robertcnelson@gmail.com>
2021-10-15 12:59:38 -0500

    build: cleanup use pwd for dir
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- build.sh
+++ build.sh
@@ -4,30 +4,20 @@
 #binutils-arm-linux-gnueabihf binutils-aarch64-linux-gnu
 #gcc-arm-linux-gnueabihf gcc-aarch64-linux-gnu
 
+DIR="$PWD"
+
 make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- distclean
 make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- j721e_evm_r5_defconfig
 make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- menuconfig
 
-if [ -f ./ti-blobs/tiboot3.bin ] ; then
-	rm -r ./ti-blobs/tiboot3.bin || true
-fi
 make ARCH=arm -j4 CROSS_COMPILE=arm-linux-gnueabihf-
 make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- savedefconfig
 cp -v defconfig ./configs/j721e_evm_r5_defconfig
-cp -v tiboot3.bin ti-blobs/
 
 make ARCH=arm CROSS_COMPILE=aarch64-linux-gnu- distclean
 make ARCH=arm CROSS_COMPILE=aarch64-linux-gnu- j721e_evm_a72_defconfig
 make ARCH=arm CROSS_COMPILE=aarch64-linux-gnu- menuconfig
 
-if [ -f ./ti-blobs/tispl.bin ] ; then
-	rm -f ./ti-blobs/tispl.bin || true
-fi
-if [ -f ./ti-blobs/u-boot.img ] ; then
-	rm -f ./ti-blobs/u-boot.img || true
-fi
-make ARCH=arm -j4 CROSS_COMPILE=aarch64-linux-gnu- ATF=ti-blobs/bl31.bin TEE=ti-blobs/tee-pager_v2.bin DM=ti-blobs/ipc_echo_testb_mcu1_0_release_strip.xer5f
+make ARCH=arm -j4 CROSS_COMPILE=aarch64-linux-gnu- ATF=${DIR}/ti-blobs/bl31.bin TEE=${DIR}/ti-blobs/tee-pager_v2.bin DM=${DIR}/ti-blobs/ipc_echo_testb_mcu1_0_release_strip.xer5f
 make ARCH=arm CROSS_COMPILE=aarch64-linux-gnu- savedefconfig
 cp -v defconfig ./configs/j721e_evm_a72_defconfig
-cp -v tispl.bin ti-blobs/
-cp -v u-boot.img ti-blobs/
