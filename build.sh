3a003bfbb570899899b86a12fd9447bd892ba3e4
Robert Nelson <robertcnelson@gmail.com>
2021-10-18 11:24:38 -0500

    config: eMMC minimize, tiboot3.bin (384 KB), tispl.bin (768 KB)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- build.sh
+++ build.sh
@@ -13,6 +13,7 @@ make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- menuconfig
 make ARCH=arm -j4 CROSS_COMPILE=arm-linux-gnueabihf-
 make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- savedefconfig
 cp -v defconfig ./configs/j721e_evm_r5_defconfig
+#ls -lh ./tiboot3.bin
 
 make ARCH=arm CROSS_COMPILE=aarch64-linux-gnu- distclean
 make ARCH=arm CROSS_COMPILE=aarch64-linux-gnu- j721e_evm_a72_defconfig
@@ -21,3 +22,5 @@ make ARCH=arm CROSS_COMPILE=aarch64-linux-gnu- menuconfig
 make ARCH=arm -j4 CROSS_COMPILE=aarch64-linux-gnu- ATF=${DIR}/ti-blobs/bl31.bin TEE=${DIR}/ti-blobs/tee-pager_v2.bin DM=${DIR}/ti-blobs/ipc_echo_testb_mcu1_0_release_strip.xer5f
 make ARCH=arm CROSS_COMPILE=aarch64-linux-gnu- savedefconfig
 cp -v defconfig ./configs/j721e_evm_a72_defconfig
+ls -lh ./tispl.bin
+ls -lh ./u-boot.bin
